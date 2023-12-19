fff63ecaf4e8b49c4796cd26f030e6db5115697a
vitaly1234567890 <Troyanvitaly123@yandex.ru>
2023-12-19 12:31:39 +0300

    Add localStorage-utils.tsx

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,5 +1,6 @@
 import {combineReducers, legacy_createStore} from "redux";
 import {counterReducer} from "./State-reducer";
+import {loadState, saveState} from "../utils/localStorage-utils";
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
@@ -7,11 +8,19 @@ import {counterReducer} from "./State-reducer";
 const rootReducer = combineReducers({
     counter: counterReducer
 })
+
 // непосредственно создаём store
-export const store = legacy_createStore(rootReducer);
+export const store = legacy_createStore(rootReducer, loadState());
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
+store.subscribe(() => {
+    saveState({
+        counter: store.getState().counter
+    })
+    localStorage.setItem('app-state', JSON.stringify(store.getState()))
+})
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
 window.store = store;
+
